5b1129c6bf8120432e4fb7826a1855c5d6a8ee2e
María-José Mejía <165747579+MajitoBrinkitos@users.noreply.github.com>
2024-12-13 11:26:14 -0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+ //dark-light mode
+ document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleBtn = document.getElementById('bd-theme');
+    const themeOptions = document.querySelectorAll('[data-bs-theme-value]');
+
+    //user's theme preference
+    const currentTheme = localStorage.getItem('theme');
+    if(currentTheme){
+        document.documentElement.setAttribute('data-bs-theme', currentTheme);
+        document.getElementById('bd-theme-text').innerText = `Toggle theme (${currentTheme})`;
+    }
+
+    //Theme toggle button
+    themeOptions.forEach(option => {
+        option.addEventListener('click', () => {
+            const theme = option.getAttribute('data-bs-theme-value');
+            document.documentElement.setAttribute('data-bs-theme', theme);
+            document.getElementById('bd-theme-text').innerText = `Toggle theme (${theme})`;
+            localStorage.setItem('theme', theme);
+
+            //update button
+            themeOptions.forEach(opt => opt.classList.remove('active'));
+            option.classList.add('active');
+        });
+    });
+ });
